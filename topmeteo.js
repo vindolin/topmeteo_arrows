240f5f028c19a33f869ccc4e446f9d0bd7f22ab0
Thomas Schüßler <vindolin@gmail.com>
2018-07-19 16:12:49 +0200

    WIP

---
+++ topmeteo.js
@@ -0,0 +1,134 @@
+// ==UserScript==
+// @name         Topmeteo Arrows
+// @namespace    http://tampermonkey.net/
+// @version      0.6.3
+// @description  Add Meteo-Parapente style arrows to the table!
+// @author       Thomas Schüßler
+// @match        https://*.topmeteo.eu/*/*/loc/*
+// @grant        none
+// ==/UserScript==
+
+{
+    'use strict';
+
+    function map(num, in_min, in_max, out_min, out_max) {
+      return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    }
+
+    const usable_height_background_color ='#FFFFAA';
+    const hsl_start = 0;
+    const hsl_end = 183; // 183 = green
+    const min_strength = 0;
+    const max_strength = 40; // km/h
+
+    const arrow_size = 20;
+    const canvas_width = 37;
+    const canvas_height = 20;
+
+    // relative to size
+    const min_arrow_head_width = 3.0;
+    const max_arrow_head_width = 7.0;
+
+    function draw_arrow(size, angle, strength) {
+        let canvas = document.createElement('canvas');
+        let ctx = canvas.getContext('2d');
+        canvas.height = size;
+        canvas.width = size;
+
+        let hue = hsl_end - map(strength, min_strength, max_strength, hsl_start, hsl_end);
+
+        let color = `hsl(${hue}, 100%, 45%)`;
+
+        ctx.strokeStyle = color;
+        ctx.fillStyle = color;
+        ctx.lineWidth = strength / 4;
+
+        // rotate around center
+        ctx.translate(size / 2, size / 2);
+        let angle_rad = angle * Math.PI / 180;
+        ctx.rotate(angle_rad);
+        ctx.translate(- size / 2, - size / 2);
+
+        ctx.moveTo(size / 2, 0);
+        ctx.lineTo(size / 2, size - size / 3);
+        ctx.stroke();
+
+        let arrow_head_width = map(strength, min_strength, max_strength, min_arrow_head_width, max_arrow_head_width);
+
+        // draw arrow head
+        ctx.beginPath();
+        ctx.moveTo(size / 2, size);
+        ctx.lineTo(size / arrow_head_width , size - size / 2.5);
+        ctx.lineTo(size - size / arrow_head_width, size - size / 2.5);
+        ctx.fill();
+
+        return canvas;
+    }
+
+    // convert strength to km/h
+    function strength_to_kmh(strength, unit) {
+        if(unit == 'km/h') {
+            return strength;
+        } else if(unit == 'kt') {
+            return strength / 0.53995680345572;
+        } else if(unit == 'm/s') {
+            return strength * 18 / 5;
+        }
+    }
+
+    let wind_pattern = /(\d+)°\/(\d+)/; // 275°/33
+    let work_heights = [];
+
+    for(let span of document.querySelectorAll('span.product-title-txt')) {
+        if(span.innerText.match(/^(Arbeitshöhe|Usable height|Altitude utilisable)/)) {
+            let tr = span.parentElement.parentElement;
+            let tds = Array.from(tr.querySelectorAll('td'));
+            let td_count = tds.length - 1;
+
+            for(let [i, td] of tds.slice(1, td_count + 1).entries()) {
+                work_heights[i] = parseInt(td.innerText);
+            }
+        }
+
+        let match = null;
+        if(match = span.innerText.match(/(?:Wind|Vent)\s+(\d+).+\[(.+)\]/)) { // Wind 2600m ISA [km/h]
+            let [, wind_height, unit] = match;
+
+            let tr = span.parentElement.parentElement;
+            let tds = Array.from(tr.querySelectorAll('td'));
+            let td_count = tds.length - 1;
+
+            // iterate over the td's, ignore the first one with text
+            for(let [i, td] of tds.slice(1, td_count + 1).entries()) {
+
+                let td_text = td.innerText.trim();
+                let [, angle, strength] = wind_pattern.exec(td_text);
+
+                angle = parseInt(angle);
+                let strength_kmh = strength_to_kmh(parseInt(strength), unit);
+
+                let canvas = document.createElement('canvas');
+                canvas.width = canvas_width;
+                canvas.height = canvas_height;
+                canvas.title = `${angle}°`;
+
+                let ctx = canvas.getContext('2d');
+                let arrow_canvas = draw_arrow(arrow_size, angle, strength_kmh);
+                ctx.drawImage(arrow_canvas, 0, 0);
+
+                // wind strength
+                ctx.font = '12px Arial, Helvetica, sans-serif';
+                ctx.fillText(strength, 22, 15);
+
+                // replace the irritating text content with our shiny new arrows
+                let td_span = td.querySelector('span');
+                td.style.padding = '0';
+                if(wind_height <= work_heights[i]) {
+                    canvas.style.backgroundColor = usable_height_background_color;
+                }
+                td.replaceChild(canvas, td_span);
+            }
+        }
+    }
+}
+a
