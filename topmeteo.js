bfe2fad742fa560c5658db9c9cd744622c6c0fa8
Thomas Schüßler <834347+vindolin@users.noreply.github.com>
2025-01-01 15:23:41 +0100

    bump version to 0.7.3 and refactor variable names for consistency and standard

--- topmeteo.js
+++ topmeteo.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Topmeteo Arrows
 // @namespace    http://tampermonkey.net/
-// @version      0.7.0
+// @version      0.7.3
 // @description  Add Meteo-Parapente style arrows to the table!
 // @author       Thomas Schüßler
 // @match        https://*.topmeteo.eu/*/*/loc/*
@@ -11,61 +11,61 @@
 {
     'use strict';
 
-    function map(num, in_min, in_max, out_min, out_max) {
-      return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+    function map(num, inMin, inMax, outMin, outMax) {
+        return (num - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
     }
 
-    const usable_height_background_color ='#FFFFAA';
-    const hsl_start = 0;
-    const hsl_end = 183; // 183 = green
-    const min_strength = 0;
-    const max_strength = 40; // km/h
+    const usableHeightBackgroundColor = '#FFFFAA';
+    const hslStart = 0;
+    const hslEnd = 183; // 183 = green
+    const minStrength = 0;
+    const maxStrength = 40; // km/h
 
-    const arrow_size = 20;
-    const canvas_width = 37;
-    const canvas_height = 20;
+    const arrowSize = 20;
+    const canvasWidth = 37;
+    const canvasHeight = 20;
 
     // relative to size
-    const min_arrow_head_width = 3.0;
-    const max_arrow_head_width = 7.0;
+    const minArrowHeadWidth = 3.0;
+    const maxArrowHeadWidth = 7.0;
 
     // only upscale
     const scale = window.devicePixelRatio > 1 ? window.devicePixelRatio : 1;
 
     function getArrowColor(strength) {
-        let hue = hsl_end - map(strength, min_strength, max_strength, hsl_start, hsl_end);
+        let hue = hslEnd - map(strength, minStrength, maxStrength, hslStart, hslEnd);
         let color = `hsl(${hue}, 100%, 45%)`;
-        if(strength >= max_strength) {
-            color = `hsl(${hsl_start}, 100%, 45%)`;
+        if (strength >= maxStrength) {
+            color = `hsl(${hslStart}, 100%, 45%)`;
         }
         return color;
     }
 
-    function draw_arrow(size, angle, strength) {
+    function drawArrow(size, angle, strength) {
         let canvas = document.createElement('canvas');
         let ctx = canvas.getContext('2d');
 
         ctx.fillStyle = getArrowColor(strength);
-        ctx.lineWidth = Math.min(strength / 4, max_strength / 3);
+        ctx.lineWidth = Math.min(strength / 4, maxStrength / 3);
 
         ctx.translate(size / 2, size / 2);
         ctx.rotate(angle * Math.PI / 180);
         ctx.translate(-size / 2, -size / 2);
 
-        let tail_width = ctx.lineWidth;
-        let arrow_head_length = map(strength, min_strength, max_strength, size / 4, size / 2);
-        let tail_height = size - arrow_head_length;
-        let arrow_head_width = map(strength, min_strength, max_strength, min_arrow_head_width, max_arrow_head_width);
+        let tailWidth = ctx.lineWidth;
+        let arrowHeadLength = map(strength, minStrength, maxStrength, size / 4, size / 2);
+        let tailHeight = size - arrowHeadLength;
+        let arrowHeadWidth = map(strength, minStrength, maxStrength, minArrowHeadWidth, maxArrowHeadWidth);
 
         // draw the arrow
         ctx.beginPath();
-        ctx.moveTo(size / 2 - tail_width / 2, 0);
-        ctx.lineTo(size / 2 + tail_width / 2, 0);
-        ctx.lineTo(size / 2 + tail_width / 2, tail_height);
-        ctx.lineTo(size - size / arrow_head_width, size - arrow_head_length);
+        ctx.moveTo(size / 2 - tailWidth / 2, 0);
+        ctx.lineTo(size / 2 + tailWidth / 2, 0);
+        ctx.lineTo(size / 2 + tailWidth / 2, tailHeight);
+        ctx.lineTo(size - size / arrowHeadWidth, size - arrowHeadLength);
         ctx.lineTo(size / 2, size);
-        ctx.lineTo(size / arrow_head_width, size - arrow_head_length);
-        ctx.lineTo(size / 2 - tail_width / 2, tail_height);
+        ctx.lineTo(size / arrowHeadWidth, size - arrowHeadLength);
+        ctx.lineTo(size / 2 - tailWidth / 2, tailHeight);
         ctx.closePath();
         ctx.fill();
         ctx.lineWidth = 0.5;
@@ -75,108 +75,108 @@
     }
 
     // convert strength to km/h
-    function strength_to_kmh(strength, unit) {
-        if(unit == 'km/h') {
+    function strengthToKmh(strength, unit) {
+        if (unit == 'km/h') {
             return strength;
-        } else if(unit == 'kt') {
+        } else if (unit == 'kt') {
             return strength * 1.852;
-        } else if(unit == 'm/s') {
+        } else if (unit == 'm/s') {
             return strength * 18 / 5;
         }
     }
 
-    let wind_pattern = /(\d+)°\/(\d+)/; // 275°/33
-    let thermal_heights = [];
+    let windPattern = /(\d+)°\/(\d+)/; // 275°/33
+    let thermalHeights = [];
 
-    function apply_arrows() {
+    function applyArrows() {
 
-        for(let span of document.querySelectorAll('span.product-title-txt')) {
+        for (let span of document.querySelectorAll('span.product-title-txt')) {
 
             // use usable height for the yellow height markers
-            if(span.innerText.match(/^(Arbeitshöhe|Usable height|Altitude utilisable)/)) {
+            if (span.innerText.match(/^(Arbeitshöhe|Usable height|Altitude utilisable)/)) {
                 let tr = span.parentElement.parentElement;
                 let tds = Array.from(tr.querySelectorAll('td'));
-                let td_count = tds.length - 1;
+                let tdCount = tds.length - 1;
 
-                for(let [i, td] of tds.slice(1, td_count + 1).entries()) {
-                    thermal_heights[i] = parseInt(td.innerText);
+                for (let [i, td] of tds.slice(1, tdCount + 1).entries()) {
+                    thermalHeights[i] = parseInt(td.innerText);
                 }
             }
 
             // overwrite with cumulus base if there are clouds
-            if(span.innerText.match(/^(Cumulus Basis|Cumulus base|Base Cumulus)/)) {
+            if (span.innerText.match(/^(Cumulus Basis|Cumulus base|Base Cumulus)/)) {
                 let tr = span.parentElement.parentElement;
                 let tds = Array.from(tr.querySelectorAll('td'));
-                let td_count = tds.length - 1;
+                let tdCount = tds.length - 1;
 
-                for(let [i, td] of tds.slice(1, td_count + 1).entries()) {
-                    let cloud_base = parseInt(td.innerText);
-                    if(cloud_base) thermal_heights[i] = cloud_base;
+                for (let [i, td] of tds.slice(1, tdCount + 1).entries()) {
+                    let cloudBaseHeight = parseInt(td.innerText);
+                    if (cloudBaseHeight) thermalHeights[i] = cloudBaseHeight;
                 }
             }
 
             let match = null;
 
-            if(match = span.innerText.match(/(?:Wind|Vent)\s+(\d+).+\[(.+)\]/)) { // Wind 2600m ISA [km/h]
-                let [, wind_height, unit] = match;
+            if (match = span.innerText.match(/(?:Wind|Vent)\s+(\d+).+\[(.+)\]/)) { // Wind 2600m ISA [km/h]
+                let [, windHeight, unit] = match;
 
                 let tr = span.parentElement.parentElement;
                 let tds = Array.from(tr.querySelectorAll('td'));
-                let td_count = tds.length - 1;
+                let tdCount = tds.length - 1;
 
                 // iterate over the td's, ignore the first one with text
-                for(let [i, td] of tds.slice(1, td_count + 1).entries()) {
+                for (let [i, td] of tds.slice(1, tdCount + 1).entries()) {
 
-                    let td_text = td.innerText.trim();
-                    let [, angle, strength] = wind_pattern.exec(td_text);
+                    let tdText = td.innerText.trim();
+                    let [, angle, strength] = windPattern.exec(tdText);
 
                     angle = parseInt(angle);
-                    let strength_kmh = strength_to_kmh(parseInt(strength), unit);
+                    let strengthKmh = strengthToKmh(parseInt(strength), unit);
 
                     let canvas = document.createElement('canvas');
-                    canvas.width = canvas_width * scale;
-                    canvas.height = canvas_height * scale;
+                    canvas.width = canvasWidth * scale;
+                    canvas.height = canvasHeight * scale;
                     canvas.title = `${angle}°`;
 
                     let ctx = canvas.getContext('2d');
 
 
-                    let arrow_canvas = draw_arrow(arrow_size * scale, angle, strength_kmh);
+                    let arrowCanvas = drawArrow(arrowSize * scale, angle, strengthKmh);
 
-                    if(scale != 1) {
+                    if (scale != 1) {
                         ctx.scale(scale, scale);
                     }
 
-                    ctx.drawImage(arrow_canvas, 0, 0);
+                    ctx.drawImage(arrowCanvas, 0, 0);
 
                     // wind strength
                     ctx.font = '12px Arial, Helvetica, sans-serif';
                     ctx.strokeStyle = 'rgba(255, 255, 255, 1)';
                     ctx.lineWidth = 4;
                     ctx.textAlign = 'right';
-                    ctx.strokeText(strength, canvas_width, 20);
+                    ctx.strokeText(strength, canvasWidth, 20);
                     ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-                    ctx.fillText(strength, canvas_width, 20);
+                    ctx.fillText(strength, canvasWidth, 20);
 
                     // replace the irritating text content with our shiny new arrows
-                    let td_span = td.querySelector('span');
+                    let tdSpan = td.querySelector('span');
 
-                    canvas.style.width = `${canvas_width}px`;
-                    canvas.style.height = `${canvas_height}px`;
+                    canvas.style.width = `${canvasWidth}px`;
+                    canvas.style.height = `${canvasHeight}px`;
 
-                    if(wind_height <= thermal_heights[i]) {
-                        canvas.style.backgroundColor = usable_height_background_color;
+                    if (windHeight <= thermalHeights[i]) {
+                        canvas.style.backgroundColor = usableHeightBackgroundColor;
                     }
 
-                    let old_arrow = td.querySelector('canvas');
+                    let oldArrow = td.querySelector('canvas');
 
                     // Hide the old span with text so that the next Ajax request still can find and modify it.
-                    td_span.style.overflow = 'hidden';
-                    td_span.style.width = '0px';
-                    td_span.style.height = '0px';
+                    tdSpan.style.overflow = 'hidden';
+                    tdSpan.style.width = '0px';
+                    tdSpan.style.height = '0px';
 
-                    if(old_arrow) { // replace the old arrow on next draws
-                        td.replaceChild(canvas, old_arrow);
+                    if (oldArrow) { // replace the old arrow on next draws
+                        td.replaceChild(canvas, oldArrow);
                     } else {  // first draw, just append
                         td.appendChild(canvas);
                     }
@@ -194,23 +194,23 @@
         subtree: true
     }
 
-    let observed_target = document.querySelector('div#forecast-table table.responsive');
+    let observedTarget = document.querySelector('div#forecast-table table.responsive');
 
-    let observer = new MutationObserver(function(mutations, observer_) {
+    let observer = new MutationObserver(function (mutations, observer_) {
         // something changed in the table!
 
         // because we mutate the table ourselves, first stop the observer
         observer_.disconnect();
 
         // draw our pretty arrows
-        apply_arrows();
+        applyArrows();
 
         // and resume observing
-        observer.observe(observed_target, config);
+        observer.observe(observedTarget, config);
     });
 
-    observer.observe(observed_target, config);
+    observer.observe(observedTarget, config);
 
     // no Ajax on first load, just parse the page
-    apply_arrows();
+    applyArrows();
 }
